8f9b1b7ca2cafdf57b752ddf147a181342d66236
pantpad <138708449+pantpad@users.noreply.github.com>
2023-09-04 12:06:01 +0200

    Generate book constructor, library arr and append

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('script working');
+
+//book object constructor
+function Book(title,author,pages,read){
+    this.title = title ?? 'generic book';
+    this.author = author ?? 'generic author';
+    this.pages = pages ?? 0;
+    this.read = read ?? false;
+}
+
+Book.prototype.info = function(){
+    console.log(this.title +","+ this.author +","+ this.pages +","+ this.read+".");
+}
+
+const firstBook = new Book("Il signore","asdsad","333",false);
+
+const myLibrary = [];
+
+function appendBookToLibrary(book){
+    myLibrary.push(book ?? new Book());
+}
